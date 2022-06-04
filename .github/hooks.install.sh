ef6e5e359ed92c720e03e51253aae3d7ec2d8269
HarvelsX <90945793+HarvelsX@users.noreply.github.com>
2022-06-04 23:46:14 +0300

    Share hooks scripts;
    Use `./.github/hooks.install.sh` for install;

---
+++ .github/hooks.install.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+readonly GIT_BASE=$(git rev-parse --show-toplevel)
+readonly HOOKS_DIR="$GIT_BASE/.git/hooks"
+readonly PUBLIC_HOOKS_DIR='./hooks'
+
+for filename in $PUBLIC_HOOKS_DIR/*
+do
+    [ -e "$filename" ] || continue
+
+    hookreal=`realpath --relative-to="$PUBLIC_HOOKS_DIR" $filename`
+    hookpath=`realpath --relative-to="$GIT_BASE" $filename`
+
+    if [ -e "$HOOKS_DIR/$hookreal" ]
+    then
+        [ -e "$HOOKS_DIR/${hookreal}.local" ] \
+            && echo "Skip creating hooks because they already exist." \
+            && echo "You can use '$HOOKS_DIR/${hookreal}.local' for your scripts" \
+            && break
+
+        echo "Rename local $hookreal hook to ${hookreal}.local"
+        mv "$HOOKS_DIR/$hookreal" "$HOOKS_DIR/${hookreal}.local"
+    fi
+    
+    cat << EOF > "$HOOKS_DIR/$hookreal"
+#!/bin/sh
+. $hookpath
+[ -e "./${hookreal}.local" ] && . ./${hookreal}.local
+exit 0
+EOF
+    echo "Added '${hookreal}' hook"
+done
